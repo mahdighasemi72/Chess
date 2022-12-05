2a8d6579296dc9651ae75edcf581e84b83def66f
mahdighasemi72 <mahdighasemi72@yahoo.com>
2022-12-05 13:16:43 +0330

    Register Menu Refactor

--- src/Menu/RegisterMenu.java
+++ src/Menu/RegisterMenu.java
@@ -15,65 +15,88 @@ public class RegisterMenu {
 
     public int Begin(String command){
         int menuNum = 1 ;
+        menuNum = executeCommand(command, menuNum);
+        return menuNum;
+    }
+
+    private int executeCommand(String command, int menuNum) {
         if (ConsoleCommand.EXIT.getStringMatcher(command).matches()){
             System.out.println(printMassage.BYE_BYE);
             menuNum = 0;
         } else if (ConsoleCommand.REGISTER.getStringMatcher(command).matches()) {
-            Matcher matcher = ConsoleCommand.REGISTER.getStringMatcher(command);
-            if (matcher.find()){
-                if (!(matcher.group(1).matches("\\w+"))){
-                    System.out.println(printMassage.INVALID_USERNAME_FORMAT);
-                }if (!(matcher.group(2).matches("\\w+"))){
-                    System.out.println(printMassage.INVALID_PASSWORD_FORMAT);
-                }
-                else {
-                    String username = matcher.group(1);
-                    String password = matcher.group(2);
-                    if (controller.playerIsExist(username)) {
-                        System.out.println(printMassage.USER_EXIST);
-                    }else {
-                        controller.addPlayer(username,password);
-                        System.out.println(printMassage.REGISTER_SUCCESSFUL);
-                    }
-                }
-            }
+            processRegister(command);
         } else if (ConsoleCommand.LOGIN.getStringMatcher(command).matches()){
-            Matcher matcher = ConsoleCommand.LOGIN.getStringMatcher(command);
-            if (matcher.find()){
-                String username = matcher.group(1);
-                String password = matcher.group(2);
-                if (!controller.playerIsExist(username)){
-                    System.out.println(printMassage.NO_USER_EXIST);
-                }else if (!controller.passwordCheck(username,password)){
-                    System.out.println(printMassage.INCORRECT_PASSWORD);
-                }else {
-                    loginPlayerUsername = username;
-                    System.out.println(printMassage.LOGIN_SUCCESSFUL);
-                    menuNum = 2;
-                }
-            }
+            menuNum = processLogin(command);
         } else if (ConsoleCommand.LIST_USERS.getStringMatcher(command).matches()) {
             System.out.println(controller.getPlayers());
         }else if (ConsoleCommand.HELPREGISTERMENU.getStringMatcher(command).matches()) {
             System.out.println(printMassage.REGISTER_MENU_HELP);
         }else if (ConsoleCommand.REMOVE.getStringMatcher(command).matches()) {
-            Matcher matcher = ConsoleCommand.REMOVE.getStringMatcher(command);
-            if (matcher.find()) {
-                String username = matcher.group(1);
-                String password = matcher.group(2);
-                if (!controller.playerIsExist(username)) {
-                    System.out.println(printMassage.NO_USER_EXIST);
-                } else if (!controller.passwordCheck(username,password)) {
-                    System.out.println(printMassage.INCORRECT_PASSWORD);
-                } else {
-                    controller.removePlayer(username,password);
-                    System.out.printf(printMassage.REMOVE,username);
-                }
-            }
+            processRemove(command);
         }
         else {
             System.out.println(printMassage.INVALID_COMMAND);
         }
         return menuNum;
+    }
+
+    private void processRemove(String command) {
+        Matcher matcher = ConsoleCommand.REMOVE.getStringMatcher(command);
+        if (matcher.find()) {
+            String username = matcher.group(1);
+            String password = matcher.group(2);
+            if (!controller.playerIsExist(username)) {
+                System.out.println(printMassage.NO_USER_EXIST);
+            } else if (!controller.passwordCheck(username,password)) {
+                System.out.println(printMassage.INCORRECT_PASSWORD);
+            } else {
+                controller.removePlayer(username,password);
+                System.out.printf(printMassage.REMOVE,username);
+            }
+        }
+    }
+
+    private int processLogin(String command) {
+        Matcher matcher = ConsoleCommand.LOGIN.getStringMatcher(command);
+        int menuNum = 1;
+        if (matcher.find()){
+            String username = matcher.group(1);
+            String password = matcher.group(2);
+            if (!controller.playerIsExist(username)){
+                System.out.println(printMassage.NO_USER_EXIST);
+            }else if (!controller.passwordCheck(username,password)){
+                System.out.println(printMassage.INCORRECT_PASSWORD);
+            }else {
+                loginPlayerUsername = username;
+                System.out.println(printMassage.LOGIN_SUCCESSFUL);
+                menuNum = 2;
+            }
+        }
+        return menuNum;
+    }
+
+    private void processRegister(String command) {
+        Matcher matcher = ConsoleCommand.REGISTER.getStringMatcher(command);
+        if (matcher.find()){
+            if (!(matcher.group(1).matches("\\w+"))){
+                System.out.println(printMassage.INVALID_USERNAME_FORMAT);
+            }if (!(matcher.group(2).matches("\\w+"))){
+                System.out.println(printMassage.INVALID_PASSWORD_FORMAT);
+            }
+            else {
+                checkUsernameAndAdd(matcher);
+            }
+        }
+    }
+
+    private void checkUsernameAndAdd(Matcher matcher) {
+        String username = matcher.group(1);
+        String password = matcher.group(2);
+        if (controller.playerIsExist(username)) {
+            System.out.println(printMassage.USER_EXIST);
+        }else {
+            controller.addPlayer(username,password);
+            System.out.println(printMassage.REGISTER_SUCCESSFUL);
+        }
     }
 }
